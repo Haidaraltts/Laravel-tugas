aadacc8a2242168ac16da89135f13d8e9ef04d23
Haidaraltts <muhamadhaidar28@gmail.com>
2024-10-16 10:48:30 +0700

    index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="assets/globals.css" />
+    <link rel="stylesheet" href="assets/styleguide.css" />
+    <link rel="stylesheet" href="assets/style.css" />
+  </head>
+  <body>
+    <div class="slide">
+      <div class="div">
+        <div class="overlap-group">
+          <img class="rectangle" src="/assets/img/Rectangle 2.png" />
+          <img class="user" src="/assets/img/user.png" />
+          <img class="settings" src="/assets/img/settings.png" />
+          <div class="aksi-damai">WAR HISTORY</div>
+          <img class="desain-tanpa-judul" src="/assets/img/Desain tanpa judul 1.png" />
+          <img class="polygon" src="/assets/img/Polygon 1.png" />
+          <img class="img" src="/assets/img/search.png" />
+        </div>
+        <div class="overlap">
+          <img class="img-2" src="/assets/img/download 1.png" />
+          <p class="text-wrapper"> perang suci dapat menyebabkan dampak yang merugikan</p>
+        </div>
+        <div class="overlap-2">
+          <img class="image" src="/assets/img/image (11).png" />
+          <p class="p">
+            Perang Salib adalah serangkaian konflik militer yang terjadi antara abad ke-11 hingga ke-13, sebagian besar
+            antara umat Kristen Eropa dan Muslim di Timur Tengah
+          </p>
+        </div>
+        <div class="overlap-3">
+          <img class="image-2" src="/assets/img/image (12).png" />
+          <p class="text-wrapper-2">
+            adalah konflik besar yang melibatkan banyak negara di Eropa, terutama di Kekaisaran Romawi Suci (Holy Roman
+            Empire), yang mencakup wilayah Jerman modern.
+          </p>
+        </div>
+        <div class="overlap-4">
+          <img class="image-3" src="/assets/img/image (13).png" />
+          <p class="salah-satu-aspeknya">
+            Salah satu aspeknya adalah ketegangan agama antara Anglikan, Puritan (Protestan), dan Katolik.<br />Perang
+            ini juga mencakup masalah politik antara parlemen dan raja.
+          </p>
+          <p class="salah-satu-aspeknya">
+            Salah satu aspeknya adalah ketegangan agama antara Anglikan, Puritan (Protestan), dan Katolik.<br />Perang
+            ini juga mencakup masalah politik antara parlemen dan raja.
+          </p>
+        </div>
+        <div class="overlap-5">
+          <div class="rectangle-2"></div>
+          <div class="rectangle-3"></div>
+          <div class="rectangle-4"></div>
+          <img class="image-4" src="/assets/img/image (19).png" />
+          <img class="image-5" src="/assets/img/image (21).png" />
+          <img class="image-6" src="/assets/img/image (17).png" />
+          <p class="konflik-antara-india">
+            Konflik antara India (beragama Hindu mayoritas) dan Pakistan (mayoritas Muslim) yang masih berlangsung.<br />Konflik
+            ini sering dikaitkan dengan agama, terutama menyangkut wilayah mayoritas Muslim di Kashmir.
+          </p>
+          <p class="juga-dikenal-sebagai">
+            Juga dikenal sebagai Perang Biafra, ini adalah konflik antara pemerintah Nigeria dan negara bagian Biafra
+            yang memisahkan diri.<br />Meski faktor utamanya adalah etnis dan politik, perang ini juga melibatkan
+            dimensi agama. Nigeria utara mayoritas Muslim, sementara Nigeria selatan mayoritas Kristen dan animis.
+          </p>
+          <p class="perang-yang-dipimpin">
+            Perang yang dipimpin oleh Sheikh Usman dan Fodio di Afrika Barat untuk mendirikan negara Islam Fulani.<br />Fokusnya
+            adalah memurnikan Islam di kawasan tersebut, melawan pengaruh tradisional lokal yang dianggap menyimpang.
+          </p>
+        </div>
+        <div class="overlap-6">
+          <img class="img-2" src="/assets/img/image (14).png" />
+          <p class="konflik-yang">
+            Konflik yang berkelanjutan di beberapa wilayah seperti Irak, Suriah, Yaman, dan Bahrain.<br />Ketegangan ini
+            sering terkait dengan perbedaan interpretasi ajaran Islam antara kelompok Sunni dan Syiah.
+          </p>
+        </div>
+        <div class="overlap-7">
+          <div class="rectangle-5"></div>
+          <div class="rectangle-6"></div>
+          <img class="image-7" src="/assets/img/image (20).png" />
+          <img class="image-8" src="/assets/img/image (16).png" />
+          <p class="serangkaian-perang">
+            Serangkaian perang saudara di Prancis yang disebabkan oleh ketegangan antara umat Katolik dan Huguenot
+            (Protestan).<br />Konflik ini diakhiri dengan Edik Nantes, yang memberikan toleransi terbatas kepada
+            Protestan.
+          </p>
+          <p class="text-wrapper-3">
+            Berbagai konflik antara umat Hindu dan Muslim sepanjang sejarah India. Termasuk perpecahan India pada tahun
+            1947 yang menimbulkan kekerasan besar-besaran antara kedua komunitas tersebut.
+          </p>
+        </div>
+        <div class="overlap-8">
+          <div class="rectangle-2"></div>
+          <img class="image-9" src="/assets/img/image (15).png" />
+          <p class="konflik-yang-2">
+            Konflik yang kompleks antara kelompok Kristen Maronit, Sunni, Syiah, dan Druze di Lebanon.<br />Perang ini
+            memiliki dimensi agama yang kuat meskipun juga melibatkan banyak aspek politik dan sosial.
+          </p>
+        </div>
+        <div class="overlap-9">
+          <p class="text-wrapper-4">
+            Hilangnya persatuan dan kesatuan di masyarakat, Hilangnya rasa keamanan dan kenyamanan di masyarakat,
+            Rusaknya hubungan antarwarga, Rusaknya tatanan sosial, Membahayakan stabilitas negara
+          </p>
+          <img class="download" src="/assets/img/download 2.png" />
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
